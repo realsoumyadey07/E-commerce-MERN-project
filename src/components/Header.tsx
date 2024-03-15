219e4607298269d90bcfe4b5d2810650c7d3c336
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-03-15 12:55:21 +0530

    product card is added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,65 +4,67 @@ import { MdShoppingCart } from "react-icons/md";
 import { PiSignInBold, PiSignOutBold } from "react-icons/pi";
 import { Link } from "react-router-dom";
 
-
-
-const user = { _id: " ", role: "admin"}
+const user = { _id: " ", role: "admin" };
 const Header = () => {
-
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-
   const dialogRef = useRef<HTMLDivElement | null>(null);
-  const handleToggleDialog = ()=> {
-     setIsOpen((prev) => !prev);
+  const handleToggleDialog = () => {
+    setIsOpen((prev) => !prev);
+  };
+  const logOutHandle = ()=> {
+    setIsOpen(false);
   }
-  useEffect(()=>{
-     const handleClickOutside = (event: MouseEvent) => {
-          if (
-            dialogRef.current &&
-            !dialogRef.current.contains(event.target as Node) &&
-            event.target !== document.querySelector('.excluded-area')
-          ) {
-            setIsOpen(false);
-          }
-        };
-    
-        document.addEventListener('mousedown', handleClickOutside);
-    
-        return () => {
-          document.removeEventListener('mousedown', handleClickOutside);
-        };
-  },[])
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent | TouchEvent) => {
+      if (
+        dialogRef.current &&
+        !dialogRef.current.contains(event.target as Node) &&
+        event.target !== document.querySelector('.excluded-area')
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // return () => {
+    //   document.removeEventListener("mousedown", handleClickOutside);
+    // };
+  }, []);
   return (
-    <nav>
+    <nav className="header">
       <Link to={"/"}>Home</Link>
       <Link to={"/search"}>
-          <FaSearch />
+        <FaSearch />
       </Link>
       <Link to={"/cart"}>
-          <MdShoppingCart />
+        <MdShoppingCart />
       </Link>
-{
-     user?._id? (
-          <div ref={dialogRef}>
+      {user?._id ? (
+        <div ref={dialogRef}>
           <a onClick={handleToggleDialog} className="excluded-area">
-               <FaUser/>
+            <FaUser />
           </a>
-          <dialog open={isOpen}>
-               {
-                    user.role==='admin' && <Link onClick={()=>setIsOpen(false)} to={'/admin/dashboard'}>Dahboard</Link> 
-               }
-               <Link onClick={()=>setIsOpen(false)} to={'/order'}>Orders</Link>
-               <button onClick={()=>setIsOpen(false)}>
-                    <PiSignOutBold />
-               </button>
+          <dialog style={{ display: isOpen ? 'flex' : 'none' }} open={isOpen}>
+            {user.role === "admin" && (
+              <Link onClick={() => setIsOpen(false)} to={"/admin/dashboard"}>
+                Dahboard
+              </Link>
+            )}
+            <Link onClick={() => setIsOpen(false)} to={"/order"}>
+              Orders
+            </Link>
+            <button onClick={logOutHandle}>
+              <PiSignOutBold />
+            </button>
           </dialog>
-
-          </div>
-     ): <Link to={'/login'}>
-          <PiSignInBold/>
-     </Link>
-}
+        </div>
+      ) : (
+        <Link to={"/login"}>
+          <PiSignInBold />
+        </Link>
+      )}
     </nav>
   );
 };
